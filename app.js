b4ec0dae47fc749fc56f56f1041a9bfc7876eec6
Aishwarya <aishwaryagusinge155@gmail.com>
2026-09-13 13:31:15 +0530

    Configure production settings for Render deployment

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate=require("ejs-mate");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL || MONGO_URL;
 const ExpressError=require("./utils/ExpressError.js");
 
 const listingRouter=require("./routes/listing.js");
@@ -17,6 +18,7 @@ const reviewRouter=require("./routes/review.js");
 const userRouter=require("./routes/user.js");
 
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -33,7 +35,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -44,20 +46,33 @@ app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 
-const sessionOptions={
-  secret: "mysupersecretcode",
-  resave:false,
-  saveUninitialized:true,
-  cookie:{
-    expires:Date.now()+1000*60*60*24*3,
-    maxAge:1000*60*60*24*3,
-    httpOnly:true
-  }
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET || "mysupersecretcode",
+  },
+  touchAfter: 24 * 3600,
+});
+
+store.on("error", (err) => {
+  console.log("ERROR in MONGO SESSION STORE", err);
+});
+
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET || "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 3,
+    maxAge: 1000 * 60 * 60 * 24 * 3,
+    httpOnly: true,
+  },
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -109,6 +124,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs",{message});
 });
 
-app.listen(8080, () => {
-  console.log("server is listening to port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`server is listening to port ${port}`);
 });
--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => {
@@ -13,14 +13,18 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
- await Listing.deleteMany({});
- initData.data=initData.data.map((obj)=>({ ...obj, owner: new mongoose.Types.ObjectId("685f9cae5b7f684f78b4e780")}));
-await Listing.insertMany(initData.data);
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: new mongoose.Types.ObjectId("685f9cae5b7f684f78b4e780"),
+  }));
+  await Listing.insertMany(initData.data);
   console.log("data was initialized");
+  await mongoose.connection.close();
 };
 
 initDB();
