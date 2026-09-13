47bdc1737c85c5214554daf28c4c2a527ef045c9
Aishwarya <aishwaryagusinge155@gmail.com>
2026-09-13 23:27:43 +0530

    Add safe template guards and server error logging

--- app.js
+++ app.js
@@ -122,7 +122,8 @@ app.all(/.*/, (req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).render("error.ejs",{message});
+  console.error("SERVER ERROR:", err);
+  res.status(statusCode).render("error.ejs", { message });
 });
 
 const port = process.env.PORT || 8080;
